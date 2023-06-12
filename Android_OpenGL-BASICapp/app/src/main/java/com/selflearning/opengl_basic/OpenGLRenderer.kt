e67df05ddc5ef2121ead7214a20417a74de07d6d
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-06-12 18:01:16 +0400

    Defined the three main functions of the 'OpenGLRenderer'.

--- Android_OpenGL-BASICapp/app/src/main/java/com/selflearning/opengl_basic/OpenGLRenderer.kt
+++ Android_OpenGL-BASICapp/app/src/main/java/com/selflearning/opengl_basic/OpenGLRenderer.kt
@@ -11,6 +11,22 @@ import javax.microedition.khronos.opengles.GL10
 class OpenGLRenderer : GLSurfaceView.Renderer {
     // We initialise the rendering here
     override fun onSurfaceCreated(unused: GL10, config: EGLConfig) {
-        TODO("Not yet implemented")
+        // Set the background colour (red=0, green=0, blue=0, alpha=1)
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+
+        // Enable depth testing - will cause nearer 3D objects to automatically
+        // be drawn over further objects
+        GLES20.glClearDepthf(1.0f)
+        GLES20.glEnable(GLES20.GL_DEPTH_TEST)
+    }
+
+    // We draw our shapes here
+    override fun onDrawFrame(unused: GL10) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT or GLES20.GL_DEPTH_BUFFER_BIT)
+    }
+
+    // Used if the screen is resized
+    override fun onSurfaceChanged(unused: GL10, w: Int, h: Int) {
+        GLES20.glViewport(0, 0, w, h)
     }
 }
